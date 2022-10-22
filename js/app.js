00acedaae9eeb30702791c11ed442c2860e8ea4b
Adriano Abdelnur <adrianoabdelnur@hotmail.com>
2022-10-22 18:12:19 -0300

    Se pone a funcionar buscador con titulo e interprete

--- js/app.js
+++ js/app.js
@@ -228,7 +228,7 @@ const mostrarCanciones = (cancionesAMostrar) => {
                             <li><a class="cardText">Nombre: ${tema.titulo}</a></li>
                             <li><a class="cardTextSecond">Autor: ${tema.interprete}</a></li>
                             <li><p class="cardTextSecond">descripción: ${tema.descripcion}</p></li>
-                            <li><button class="btn btn-outline-primary meGusta" onclick="like(${tema.id})">
+                            <li><button class="btn btn-outline-primary meGusta"(${tema.id})">
                             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-emoji-heart-eyes-fill" viewBox="0 0 16 16">
                             <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zM4.756 4.566c.763-1.424 4.02-.12.952 3.434-4.496-1.596-2.35-4.298-.952-3.434zm6.559 5.448a.5.5 0 0 1 .548.736A4.498 4.498 0 0 1 7.965 13a4.498 4.498 0 0 1-3.898-2.25.5.5 0 0 1 .548-.736h.005l.017.005.067.015.252.055c.215.046.515.108.857.169.693.124 1.522.242 2.152.242.63 0 1.46-.118 2.152-.242a26.58 26.58 0 0 0 1.109-.224l.067-.015.017-.004.005-.002zm-.07-5.448c1.397-.864 3.543 1.838-.953 3.434-3.067-3.554.19-4.858.952-3.434z"/>
                             </svg>
@@ -396,6 +396,18 @@ const createLista = () => {
         console.log(listaPropia);
         mostrarCanciones(listaPropia);
     }
+
+    const buscar = (filtro) => {
+        temas = JSON.parse(localStorage.getItem('temas'));
+        console.log(temas);
+        const temasFiltrados = temas.filter((tema) => 
+        (tema?.titulo?.toLowerCase()?.includes(filtro?.toLowerCase()))
+        ||
+        (tema?.interprete?.toLowerCase()?.includes(filtro?.toLowerCase()))
+        );
+        mostrarCanciones(temasFiltrados);
+    
+    };
 
 
 /** -------------------------------------------------------------------------------- */
